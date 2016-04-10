52266b125a77a0cc90f9087fa055e658596f96f7
liuhonger <1277314169@qq.com>
2016-04-10 22:07:39 +0800

    this is first cxf

--- cxf_client/src/main/java/com/WebServiceTest.java
+++ cxf_client/src/main/java/com/WebServiceTest.java
@@ -2,8 +2,13 @@ package com;
 
 import com.test.ws.service.WSTestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.annotation.Resources;
 
 
 /**
@@ -12,12 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/weServiceTest")
 public class WebServiceTest {
-    @Autowired
+    @Resource(name = "wsTestClinetBean")
     private WSTestService wsTestServicel;
 
     @RequestMapping(value = "/show")
     public void show(){
-        wsTestServicel.getSomething("aa");
-        System.out.println("bb");
+        String aa = wsTestServicel.getSomething("aa");
+        System.out.println("bb"+aa);
     }
 }
